82884b7ab0d8c3a58f9b471683c1353fc3b2e2e1
Triptofano <aho.monteiro@gmail.com>
2016-06-12 22:40:05 -0300

    M5EX1

---
+++ module/market/config/module.config.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Market;
+
+return array(
+    'router' => array(
+        'routes' => array(
+            
+            'market' => array(
+                'type'    => 'Literal',
+                'options' => array(
+                    'route'    => '/market',
+                    'defaults' => array(
+                        'controller'    => 'Market\Controller\Index',
+                        'action'        => 'index',
+                    ),
+                ),
+                'may_terminate' => true,
+                'child_routes' => array(
+                    'default' => array(
+                        'type'    => 'Segment',
+                        'options' => array(
+                            'route'    => '/[:controller[/:action]]',
+                            'constraints' => array(
+                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+                            ),
+                            'defaults' => array(
+                            ),
+                        ),
+                    ),
+                ),          
+            ),
+        ),
+    ),
+    
+    'service_manager' => array(
+        
+    ),
+    
+    'controllers' => array(
+        'invokables' => array(
+            'Market\Controller\Index' => Controller\IndexController::class
+        ),
+    ),
+    
+    'view_manager' => array(
+        'display_not_found_reason' => true,
+        'display_exceptions'       => true,
+        'doctype'                  => 'HTML5',
+        'not_found_template'       => 'error/404',
+        'exception_template'       => 'error/index',
+        'template_map' => array(
+            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+            'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
+            'error/404'               => __DIR__ . '/../view/error/404.phtml',
+            'error/index'             => __DIR__ . '/../view/error/index.phtml',
+        ),
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+    ),
+    
+);
